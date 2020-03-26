32d1ae064f24d9ba67b4f9b7ac5bed03f68cd16e
Matthew Johnson <super66reaper@gmail.com>
2020-03-26 17:04:53 -0600

    finished activity 1

--- src/CardTester.java
+++ src/CardTester.java
@@ -1,13 +1,8 @@
-/**
- * This is a class that tests the Card class.
- */
 public class CardTester {
 
-	/**
-	 * The main method in this class checks the Card operations for consistency.
-	 *	@param args is not used.
-	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("3", "Clubs", 3);
+		Card card2 = new Card("6", "Diamonds", 6);
+		Card card3 = new Card("Ace", "Spades", 1);
 	}
 }
